5fc12ca2ef4b3da03a713c3e8cd14d7ce4362c6d
German Montenegro <germontenegro715@gmail.com>
2022-10-15 10:29:11 +0200

    tarea

---
+++ mi-carro/src/components/Item.js
@@ -0,0 +1,11 @@
+function Item(p){
+    return(
+     <div>
+        <h2> {p.precio}</h2>
+     </div>
+    )
+}
+
+export default Item;
+
+
---
+++ mi-carro/src/components/ItemList.js
@@ -0,0 +1,6 @@
+const listaproductos=[{
+    ide:"",
+    nombre:"Abu Garcia",
+    precio:10000,
+    imagen:"https://d3ugyf2ht6aenh.cloudfront.net/stores/906/028/products/promax1-7f747d4211f39c544716265297916565-480-0.jpg"
+}]
